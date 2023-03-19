6f7072c1fd2f4dc6dff68d853f034204b7a9ea6f
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-03-19 12:32:28 +0530

    Update index.js

--- first_react_code/index.js
+++ first_react_code/index.js
@@ -2,6 +2,11 @@
 //IT may not show in browser cause i have used codesanbox 
 //uploded in github just for reference
 
+// import React from "react";
+// import ReactDOM from "react-dom";
+ 
+OR 
+
 var React = require("react");                   //dependencies
 var ReactDOM = require("react-dom");
 
