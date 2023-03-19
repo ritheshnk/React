6bf948dbbbded8030663c1519f269fde321c0c96
nkrithesh <nkrithesh@gmail.com>
2023-03-19 11:33:12 +0530

    first commit

---
+++ first_react_code/index.js
@@ -0,0 +1,8 @@
+
+//IT may not show in browser cause i have used codesanbox 
+//uploded in github just for reference
+
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(<h1>hello</h1>, document.getElementById("root"));
