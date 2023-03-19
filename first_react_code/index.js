cdc09d92b2b79c16ea01f0d475da131a4ba0184f
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-03-19 11:35:20 +0530

    Update index.js

--- first_react_code/index.js
+++ first_react_code/index.js
@@ -2,7 +2,7 @@
 //IT may not show in browser cause i have used codesanbox 
 //uploded in github just for reference
 
-var React = require("react");
+var React = require("react");                   //dependencies
 var ReactDOM = require("react-dom");
 
 ReactDOM.render(<h1>hello</h1>, document.getElementById("root"));
