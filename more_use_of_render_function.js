513b64b6b34bcd86ca6ffdf393e4f0bc46e26ec8
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-03-29 20:47:26 +0530

    Create more_use_of_render_function.js

---
+++ more_use_of_render_function.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>"MY MOOD"</h1>
+    <ul>
+      <li>Happy</li>
+      <li>Sad</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
