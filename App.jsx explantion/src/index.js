5ed3c2d2de60274f944ebbae067068131052a2f0
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-04-21 22:56:15 +0530

    Update index.js

--- App.jsx explantion/src/index.js
+++ App.jsx explantion/src/index.js
@@ -1,9 +1,17 @@
-import React from "react"
-import ReactDOM  from "react-dom"
-import App from "./App"
-import Greet from "./greeting"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Greet from "./greeting";
 
-
-
-
-ReactDOM.render(<h1><Greet /></h1>,document.getElementById("root"))
+ReactDOM.render(
+  <div>
+    <h1>
+      <Greet />
+    </h1>
+    <h1>
+      {" "}
+      <App />
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
